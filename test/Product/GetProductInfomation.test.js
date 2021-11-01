9ea0c70ab2ff4f64c1bc0dfa0dd900574718b9a7
Hien <hien01247@gmail.com>
2021-11-01 15:22:54 +0700

    Test[Product-1]

---
+++ test/Product/GetProductInfomation.test.js
@@ -0,0 +1,19 @@
+import { Selector, Role } from 'testcafe';
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const product_item = Selector('[data-test="productCard"]');
+const faEyeBtn = Selector('[data-test="fa-eye-btn"]');
+
+
+fixture.only('Fixture').page(process.env.URL_TEST)
+
+test('display',async (t) => {
+        const  product_name = await Selector('[data-test="product_name"]')().innerText;
+        await t.click(product_item);
+        await t.click(faEyeBtn);
+        debug();
+        const string = await Selector('[data-test="name_detail"]')().innerText;
+        await t.expect(product_name).eql(string);
+});
