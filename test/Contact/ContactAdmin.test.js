eb00f2eff198a220c720a466f657284049673660
minh286 <phamnhatminh2000@gmail.com>
2021-12-12 19:51:17 +0700

    code

---
+++ test/Contact/ContactAdmin.test.js
@@ -0,0 +1,24 @@
+import { Selector } from 'testcafe';
+import { client } from '../utilities/createRole';
+
+// import environment variables
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const user_name = Selector('[data-test="user_name"]');
+const user_email = Selector('[data-test="user_email"]');
+const user_message = Selector('[data-test="user_message"]');
+const btnSend = Selector('[data-test="send_message"]');
+
+fixture('Contact')
+    .page(`${process.env.URL_TEST}\contact`)
+
+test('Contact1', async t =>{
+        await t
+        .setNativeDialogHandler(() => true)
+        .typeText(user_name,'a')
+        .typeText(user_email, 'a@gmail.com')
+        .typeText(user_message,'Lop hoc kiem chung phan mem')
+        .click(btnSend)
+        .expect(getLocation()).contains(`${process.env.URL_TEST}\contact`);
+    })
